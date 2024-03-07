7402175e3955cb47f080bf172dacf0ce2f180f6f
Oliver <oliver.a.jarjosa@gmail.com>
2024-03-07 18:36:20 -0500

    push

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,25 @@
 const User = require('../models/User');
 
+//handleErrors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+
+    //duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'That email is already registered';
+        return errors;
+    }
+
+    //validation errors
+    if(err.message.includes('user validation failed')) {
+       Object.values(err.errors).forEach(({properties}) => {
+           errors[properties.path] = 'properties.message'
+        });
+    }
+    return errors;
+}
+
 module.exports.signup_get = 
 (req, res)=> {
     res.render('signup');
@@ -19,8 +39,8 @@ const user = await User.create({ email, password });
 res.status(201).json(user);
 }
 catch (err) {
-console.log(err);
-res.status(400).send('error: user not created.');
+handleErrors(err)
+res.status(400).json({errors});
 }
 
     console.log(email, password) 
--- models/User.js
+++ models/User.js
@@ -1,18 +1,34 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a password'],
         unique: true,
-        minLength: 6
+        minlength: [6, 'Minimum password length is 6 characters']
     },
+});
+
+//fire a function after doc saved db
+userSchema.post('save', function (doc, next) {
+console.log('new user was created and saved', doc);
+})
+
+//fire a function before a doc is saved to db
+userSchema.pre('save', async function (next) {
+   console.log('user about to be created and save', this);
+   const salt = await bcrypt.genSalt();
+   this.password = await bcrypt.hash(this.password, salt)
+   next();
 });
 
 const User = mongoose.model('user', userSchema); //must be singular
